75d624942196cd7d5edf1b48b9a3873d60a655dd
Darsh Doshi <darshdoshi01@gmail.com>
2024-03-26 18:43:40 +0530

    Created Threads Validations

---
+++ lib/validations/thread.ts
@@ -0,0 +1,9 @@
+import * as z from "zod";
+
+export const ThreadValidation = z.object({
+  name: z.string().min(3, { message: "Minimum 3 Characters" }),
+  accountId: z.string(),
+});
+export const CommentValidation = z.object({
+  name: z.string().min(3, { message: "Minimum 3 Characters" }),
+});
