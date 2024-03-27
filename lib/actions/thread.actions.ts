a56042214f1d5d70f89af2a0fc490fcae5f12a90
darsh098 <darshdoshi01@gmail.com>
2024-03-27 15:24:10 +0530

    Added fetchPosts Action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -10,6 +10,7 @@ interface Params {
   communityId: string | null;
   path: string;
 }
+
 export async function createThread({
   text,
   author,
@@ -35,4 +36,36 @@ export async function createThread({
   } catch (error: any) {
     throw new Error(`Error Creating Thread: ${error.message}`);
   }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate The Number Of Posts To Skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Fetch The Posts That Have No Parents(Top-Level Threads)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId, image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
