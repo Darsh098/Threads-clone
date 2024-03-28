2a28d3fa8da35b349b58215384429ae68a412665
darsh098 <darshdoshi01@gmail.com>
2024-03-28 12:17:12 +0530

    Bug Fix: Label Name Will Be Hidden In Small Screen

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -25,7 +25,7 @@ function Bottombar() {
                 width={24}
                 height={24}
               />
-              <p className="text-subtle-medium text-light-1 max-small:hidden">
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
                 {link.label.split(/\s+./)[0]}
               </p>
             </Link>
