8589fb2d22a37003a6be41f5ddccc35a2785a5d2
Darsh Doshi <darshdoshi01@gmail.com>
2024-04-02 21:14:57 +0530

    Removed Topbar Component

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,5 @@
-import Topbar from "@/components/shared/Topbar";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <>
-      <Topbar />
-      <SignIn />
-    </>
-  );
+  return <SignIn />;
 }
