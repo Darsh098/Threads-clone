9729ef6539eefa461d0723b480cb692c5b267a6a
Darsh Doshi <darshdoshi01@gmail.com>
2024-04-02 19:02:10 +0530

    Used DeleteThread Component

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,7 +1,7 @@
-import { fetchCommunityDetailsById } from "@/lib/actions/community.actions";
 import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import DeleteThread from "../forms/DeleteThread";
 
 interface Props {
   id: string;
@@ -13,7 +13,11 @@ interface Props {
     name: string;
     image: string;
   };
-  community: string | null;
+  community: {
+    id: string;
+    name: string;
+    image: string;
+  } | null;
   createdAt: string;
   comments: {
     author: {
@@ -23,7 +27,7 @@ interface Props {
   isComment?: boolean;
 }
 
-const ThreadCard = async ({
+const ThreadCard = ({
   id,
   currentUserId,
   parentId,
@@ -34,10 +38,6 @@ const ThreadCard = async ({
   comments,
   isComment,
 }: Props) => {
-  let communityDetails;
-  if (community) {
-    communityDetails = await fetchCommunityDetailsById(community);
-  }
   return (
     <article
       className={`flex w-full flex-col rounded-xl ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"}`}
@@ -100,28 +100,56 @@ const ThreadCard = async ({
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
                   <p className="mt-1 text-subtle-medium text-gray-1">
-                    {comments.length} replies
+                    {comments.length} repl{comments.length > 1 ? "ies" : "y"}
                   </p>
                 </Link>
               )}
             </div>
           </div>
         </div>
+        <DeleteThread
+          threadId={JSON.stringify(id)}
+          currentUserId={currentUserId}
+          authorId={author.id}
+          parentId={parentId}
+          isComment={isComment}
+        />
       </div>
-      {/* TODO: Delete A Thread */}
 
-      {!isComment && communityDetails && (
+      {!isComment && comments.length > 0 && (
+        <div className="ml-1 mt-3 flex items-center gap-2">
+          {comments.slice(0, 2).map((comment, index) => (
+            <Image
+              key={index}
+              src={comment.author.image}
+              alt={`user_${index}`}
+              width={24}
+              height={24}
+              className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+            />
+          ))}
+
+          <Link href={`/thread/${id}`}>
+            <p className="mt-1 text-subtle-medium text-gray-1">
+              {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+            </p>
+          </Link>
+        </div>
+      )}
+
+      {!isComment && community && (
         <Link
-          href={`/communities/${communityDetails.id}`}
+          href={`/communities/${community.id}`}
           className="mt-5 flex items-center"
         >
           <p className="text-subtle-medium text-gray-1">
-            {formatDateString(createdAt)} - {communityDetails.name} Community
+            {formatDateString(createdAt)}
+            {community && ` - ${community.name} Community`}
           </p>
 
           <Image
-            src={communityDetails.image}
-            alt={communityDetails.name}
+            src={community.image}
+            alt={community.name}
             width={14}
             height={14}
             className="ml-1 rounded-full object-cover"
