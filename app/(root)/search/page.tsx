181f5c9803d619f25317d686a3107d9b80c2d79c
Darsh Doshi <darshdoshi01@gmail.com>
2024-04-02 18:18:07 +0530

    Using Searchbar And Pagination Component To Search Users

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,15 @@
 import UserCard from "@/components/cards/UserCard";
+import Pagination from "@/components/shared/Pagination";
+import Searchbar from "@/components/shared/Searchbar";
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-async function Page() {
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null;
 
@@ -13,8 +19,8 @@ async function Page() {
   // Fetch Users
   const result = await fetchUsers({
     userId: user.id,
-    searchString: "",
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
@@ -22,11 +28,11 @@ async function Page() {
     <section>
       <h1 className="head-text mb-10">Search</h1>
 
-      {/* Search Bar */}
+      <Searchbar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
-          <p className="no-result">No Users Yet...</p>
+          <p className="no-result">No Users Found</p>
         ) : (
           <>
             {result.users.map((person) => (
@@ -37,11 +43,17 @@ async function Page() {
                 username={person.username}
                 imgUrl={person.image}
                 personType="User"
+                showButton={true}
               />
             ))}
           </>
         )}
       </div>
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
   );
 }
