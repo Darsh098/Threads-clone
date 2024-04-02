bc810ccbce4455e6a511e06eb95c19f4ca3f34fc
Darsh Doshi <darshdoshi01@gmail.com>
2024-04-02 17:36:25 +0530

    Added Suggested Communities And Similar Minds

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface Props {
   id: string;
@@ -9,30 +10,67 @@ interface Props {
   username: string;
   imgUrl: string;
   personType: string;
+  showButton: boolean;
 }
-const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
+const UserCard = ({
+  id,
+  name,
+  username,
+  imgUrl,
+  personType,
+  showButton,
+}: Props) => {
   const router = useRouter();
+  const isCommunity = personType === "Community";
+
   return (
     <article className="user-card">
       <div className="user-card_avatar">
-        <Image
-          src={imgUrl}
-          alt="Logo"
-          width={48}
-          height={48}
-          className="rounded-full"
-        />
+        <div className="relative h-12 w-12">
+          <Image
+            src={imgUrl}
+            alt="Profile Photo"
+            fill
+            className="rounded-full object-cover"
+          />
+        </div>
         <div className="flex-1 text-ellipsis">
-          <h4 className="text-base-semibold text-light-1">{name}</h4>
-          <p className="text-small-medium text-gray-1">@{username}</p>
+          {showButton === false ? (
+            <Link href={isCommunity ? `/communities/${id}` : `/profile/${id}`}>
+              <h4 className="text-base-semibold text-light-1">{name}</h4>
+              <p className="text-small-medium text-gray-1">@{username}</p>
+            </Link>
+          ) : (
+            <>
+              <h4 className="text-base-semibold text-light-1">{name}</h4>
+              <p className="text-small-medium text-gray-1">@{username}</p>
+            </>
+          )}
+          {/* <h4 className="text-base-semibold text-light-1">{name}</h4>
+          <p className="text-small-medium text-gray-1">@{username}</p> */}
         </div>
       </div>
-      <Button
+
+      {showButton !== false && (
+        <Button
+          className="user-card_btn"
+          onClick={() => {
+            if (isCommunity) {
+              router.push(`/communities/${id}`);
+            } else {
+              router.push(`/profile/${id}`);
+            }
+          }}
+        >
+          View
+        </Button>
+      )}
+      {/* <Button
         className="user-card_btn"
         onClick={() => router.push(`/profile/${id}`)}
       >
         View
-      </Button>
+      </Button> */}
     </article>
   );
 };
